86cfe683467601cc976ad601b7db5ec26365637a
Drew Blessing <drew@blessing.io>
2015-07-27 10:36:45 -0500

    Add LICENSE and license header to all files. Fixes #1

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,28 +2,18 @@
 # Cookbook Name:: tomcat
 # Recipe:: default
 #
-# Copyright (c) 2015 Drew A. Blessing, All Rights Reserved.
-
-apache_tomcat 'tomcat'
-
-apache_tomcat_instance 'foo'
-
-# apache_tomcat_config 'connector-9080' do
-#   type :entity
-#   instance 'foo'
-#   config_content 'foo'
-# end
+# Copyright 2015 Drew A. Blessing
 #
-# apache_tomcat_config 'server' do
-#   type :server
-#   instance 'foo'
-#   config_options do
-#     include_defaults false
-#     include_default_listeners true
-#     include_default_engine true
-#     server_port 9005
-#     entities ['connector-9080']
-#   end
-# end
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-apache_tomcat_service 'foo'
+# Recipe intentionally left blank. Use resources instead.
